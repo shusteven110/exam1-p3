555f1a6683b2acf8f63d58f84ac3de90667dc9f4
Hsu <shusteven110@gmail.com>
2023-04-17 11:34:05 +0800

    update

---
+++ SobelFilter.cpp
@@ -0,0 +1,103 @@
+#include <cmath>
+#include <iomanip>
+
+#include "SobelFilter.h"
+
+SobelFilter::SobelFilter(sc_module_name n)
+    : sc_module(n), t_skt("t_skt"), base_offset(0) {
+  SC_THREAD(do_filter);
+
+  t_skt.register_b_transport(this, &SobelFilter::blocking_transport);
+}
+
+// sobel mask
+const int mask[MASK_N][MASK_X][MASK_Y] = {{{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}},
+
+                                          {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}}};
+
+void SobelFilter::do_filter() {
+  while (true) {
+    // for (unsigned int i = 0; i < MASK_N; ++i) {
+    //   val[i] = 0;
+    // }
+    // for (unsigned int v = 0; v < MASK_Y; ++v) {
+    //   for (unsigned int u = 0; u < MASK_X; ++u) {
+    //     unsigned char grey = (i_r.read() + i_g.read() + i_b.read()) / 3;
+    //     for (unsigned int i = 0; i != MASK_N; ++i) {
+    //       val[i] += grey * mask[i][u][v];
+    //     }
+    //   }
+    // }
+    float temp1 = i_r.read();
+    float temp2 = i_r.read();
+    float temp3 = i_r.read();
+    float result = (temp1/6) + (temp2/3) + (temp3/2);
+    // double total = 0;
+    // for (unsigned int i = 0; i != MASK_N; ++i) {
+    //   total += val[i] * val[i];
+    // }
+    // int result = (int)(std::sqrt(total));
+    o_result.write(result);
+    printf("%f\n",result);
+    wait(3); //emulate module delay
+  }
+}
+
+void SobelFilter::blocking_transport(tlm::tlm_generic_payload &payload,
+                                     sc_core::sc_time &delay) {
+  sc_dt::uint64 addr = payload.get_address();
+  addr = addr - base_offset;
+  unsigned char *mask_ptr = payload.get_byte_enable_ptr();
+  unsigned char *data_ptr = payload.get_data_ptr();
+  word buffer;
+  switch (payload.get_command()) {
+  case tlm::TLM_READ_COMMAND:
+    switch (addr) {
+    case SOBEL_FILTER_RESULT_ADDR:
+      buffer.uint = o_result.read();
+      break;
+    case SOBEL_FILTER_CHECK_ADDR:
+      buffer.uint = o_result.num_available();
+      break;
+    default:
+      std::cerr << "Error! SobelFilter::blocking_transport: address 0x"
+                << std::setfill('0') << std::setw(8) << std::hex << addr
+                << std::dec << " is not valid" << std::endl;
+      break;
+    }
+    data_ptr[0] = buffer.uc[0];
+    data_ptr[1] = buffer.uc[1];
+    data_ptr[2] = buffer.uc[2];
+    data_ptr[3] = buffer.uc[3];
+    break;
+
+  case tlm::TLM_WRITE_COMMAND:
+    switch (addr) {
+    case SOBEL_FILTER_R_ADDR:
+      if (mask_ptr[0] == 0xff) {
+        i_r.write(data_ptr[0]);
+      }
+      // if (mask_ptr[1] == 0xff) {
+      //   i_g.write(data_ptr[1]);
+      // }
+      // if (mask_ptr[2] == 0xff) {
+      //   i_b.write(data_ptr[2]);
+      // }
+      break;
+    default:
+      std::cerr << "Error! SobelFilter::blocking_transport: address 0x"
+                << std::setfill('0') << std::setw(8) << std::hex << addr
+                << std::dec << " is not valid" << std::endl;
+      break;
+    }
+    break;
+
+  case tlm::TLM_IGNORE_COMMAND:
+    payload.set_response_status(tlm::TLM_GENERIC_ERROR_RESPONSE);
+    return;
+  default:
+    payload.set_response_status(tlm::TLM_GENERIC_ERROR_RESPONSE);
+    return;
+  }
+  payload.set_response_status(tlm::TLM_OK_RESPONSE); // Always OK
+}
---
+++ Testbench.cpp
@@ -0,0 +1,199 @@
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+#include "Testbench.h"
+
+const float x_input_signal[]={0.500,   0.525,   0.549,   0.574,   0.598,   0.622,   0.646,   0.670,
+                            0.693,   0.715,   0.737,   0.759,   0.780,   0.800,   0.819,   0.838,
+                            0.856,   0.873,   0.889,   0.904,   0.918,   0.931,   0.943,   0.954,
+                            0.964,   0.972,   0.980,   0.986,   0.991,   0.995,   0.998,   1.000,
+                            1.000,   0.999,   0.997,   0.994,   0.989,   0.983,   0.976,   0.968,
+                            0.959,   0.949,   0.937,   0.925,   0.911,   0.896,   0.881,   0.864,
+                            0.847,   0.829,   0.810,   0.790,   0.769,   0.748,   0.726,   0.704,
+                            0.681,   0.658,   0.634,   0.610,   0.586,   0.562,   0.537,   0.512,
+                            0.488,   0.463,   0.438,   0.414,   0.390,   0.366,   0.342,   0.319,
+                            0.296,   0.274,   0.252,   0.231,   0.210,   0.190,   0.171,   0.153,
+                            0.136,   0.119,   0.104,   0.089,   0.075,   0.063,   0.051,   0.041,
+                            0.032,   0.024,   0.017,   0.011,   0.006,   0.003,   0.001,   0.000,
+                            0.000,   0.002,   0.005,   0.009,   0.014,   0.020,   0.028,   0.036,
+                            0.046,   0.057,   0.069,   0.082,   0.096,   0.111,   0.127,   0.144,
+                            0.162,   0.181,   0.200,   0.220,   0.241,   0.263,   0.285,   0.307,
+                            0.330,   0.354,   0.378,   0.402,   0.426,   0.451,   0.475,   0.500};
+
+unsigned char header[54] = {
+    0x42,          // identity : B
+    0x4d,          // identity : M
+    0,    0, 0, 0, // file size
+    0,    0,       // reserved1
+    0,    0,       // reserved2
+    54,   0, 0, 0, // RGB data offset
+    40,   0, 0, 0, // struct BITMAPINFOHEADER size
+    0,    0, 0, 0, // bmp width
+    0,    0, 0, 0, // bmp height
+    1,    0,       // planes
+    24,   0,       // bit per pixel
+    0,    0, 0, 0, // compression
+    0,    0, 0, 0, // data size
+    0,    0, 0, 0, // h resolution
+    0,    0, 0, 0, // v resolution
+    0,    0, 0, 0, // used colors
+    0,    0, 0, 0  // important colors
+};
+
+Testbench::Testbench(sc_module_name n)
+    : sc_module(n), initiator("initiator"), output_rgb_raw_data_offset(54) {
+  SC_THREAD(do_sobel);
+}
+
+int Testbench::read_bmp(string infile_name) {
+  FILE *fp_s = NULL; // source file handler
+  fp_s = fopen(infile_name.c_str(), "rb");
+  if (fp_s == NULL) {
+    printf("fopen %s error\n", infile_name.c_str());
+    return -1;
+  }
+  // move offset to 10 to find rgb raw data offset
+  fseek(fp_s, 10, SEEK_SET);
+  assert(fread(&input_rgb_raw_data_offset, sizeof(unsigned int), 1, fp_s));
+
+  // move offset to 18 to get width & height;
+  fseek(fp_s, 18, SEEK_SET);
+  assert(fread(&width, sizeof(unsigned int), 1, fp_s));
+  assert(fread(&height, sizeof(unsigned int), 1, fp_s));
+
+  // get bit per pixel
+  fseek(fp_s, 28, SEEK_SET);
+  assert(fread(&bits_per_pixel, sizeof(unsigned short), 1, fp_s));
+  bytes_per_pixel = bits_per_pixel / 8;
+
+  // move offset to input_rgb_raw_data_offset to get RGB raw data
+  fseek(fp_s, input_rgb_raw_data_offset, SEEK_SET);
+
+  source_bitmap =
+      (unsigned char *)malloc((size_t)width * height * bytes_per_pixel);
+  if (source_bitmap == NULL) {
+    printf("malloc images_s error\n");
+    return -1;
+  }
+
+  target_bitmap =
+      (unsigned char *)malloc((size_t)width * height * bytes_per_pixel);
+  if (target_bitmap == NULL) {
+    printf("malloc target_bitmap error\n");
+    return -1;
+  }
+
+  printf("Image width=%d, height=%d\n", width, height);
+  assert(fread(source_bitmap, sizeof(unsigned char),
+               (size_t)(long)width * height * bytes_per_pixel, fp_s));
+  fclose(fp_s);
+  return 0;
+}
+
+int Testbench::write_bmp(string outfile_name) {
+  FILE *fp_t = NULL;      // target file handler
+  unsigned int file_size; // file size
+
+  fp_t = fopen(outfile_name.c_str(), "wb");
+  if (fp_t == NULL) {
+    printf("fopen %s error\n", outfile_name.c_str());
+    return -1;
+  }
+
+  // file size
+  file_size = width * height * bytes_per_pixel + output_rgb_raw_data_offset;
+  header[2] = (unsigned char)(file_size & 0x000000ff);
+  header[3] = (file_size >> 8) & 0x000000ff;
+  header[4] = (file_size >> 16) & 0x000000ff;
+  header[5] = (file_size >> 24) & 0x000000ff;
+
+  // width
+  header[18] = width & 0x000000ff;
+  header[19] = (width >> 8) & 0x000000ff;
+  header[20] = (width >> 16) & 0x000000ff;
+  header[21] = (width >> 24) & 0x000000ff;
+
+  // height
+  header[22] = height & 0x000000ff;
+  header[23] = (height >> 8) & 0x000000ff;
+  header[24] = (height >> 16) & 0x000000ff;
+  header[25] = (height >> 24) & 0x000000ff;
+
+  // bit per pixel
+  header[28] = bits_per_pixel;
+
+  // write header
+  fwrite(header, sizeof(unsigned char), output_rgb_raw_data_offset, fp_t);
+
+  // write image
+  fwrite(target_bitmap, sizeof(unsigned char),
+         (size_t)(long)width * height * bytes_per_pixel, fp_t);
+
+  fclose(fp_t);
+  return 0;
+}
+
+void Testbench::do_sobel() {
+  // int x, y, v, u;        // for loop counter
+  // unsigned char R, G, B; // color of R, G, B
+  // int adjustX, adjustY, xBound, yBound;
+  // int total;
+  float R;
+  word data;
+  unsigned char mask[4];
+  //wait(5 * CLOCK_PERIOD, SC_NS);
+  for (y = 0; y != height; ++y) {
+    for (x = 0; x != width; ++x) {
+      adjustX = (MASK_X % 2) ? 1 : 0; // 1
+      adjustY = (MASK_Y % 2) ? 1 : 0; // 1
+      xBound = MASK_X / 2;            // 1
+      yBound = MASK_Y / 2;            // 1
+
+  for(int i=0;i<128;i++){
+    for(int j=-1;j<=1;j++){
+      if(i+j<0 || i+j>=128){
+        R = 0;
+      }
+      else{
+        R = x_input_signal[i+j];
+      }
+          data.uc[0] = R;
+          mask[0] = 0xff;
+          mask[1] = 0;
+          mask[2] = 0;
+          mask[3] = 0;
+          initiator.write_to_socket(SOBEL_FILTER_R_ADDR, mask, data.uc, 4);
+          wait(1 * CLOCK_PERIOD, SC_NS);
+        }
+      }
+
+    //   bool done=false;
+    //   int output_num=0;
+    //   while(!done){
+    //     initiator.read_from_socket(SOBEL_FILTER_CHECK_ADDR, mask, data.uc, 4);
+    //     output_num = data.sint;
+    //     if(output_num>0) done=true;
+    //   }
+    //   wait(10 * CLOCK_PERIOD, SC_NS);
+    //   initiator.read_from_socket(SOBEL_FILTER_RESULT_ADDR, mask, data.uc, 4);
+    //   total = data.sint;
+    //   //debug
+    //   //cout << "Now at " << sc_time_stamp() << endl; //print current sc_time
+
+    //   if (total - THRESHOLD >= 0) {
+    //     // black
+    //     *(target_bitmap + bytes_per_pixel * (width * y + x) + 2) = BLACK;
+    //     *(target_bitmap + bytes_per_pixel * (width * y + x) + 1) = BLACK;
+    //     *(target_bitmap + bytes_per_pixel * (width * y + x) + 0) = BLACK;
+    //   } else {
+    //     // white
+    //     *(target_bitmap + bytes_per_pixel * (width * y + x) + 2) = WHITE;
+    //     *(target_bitmap + bytes_per_pixel * (width * y + x) + 1) = WHITE;
+    //     *(target_bitmap + bytes_per_pixel * (width * y + x) + 0) = WHITE;
+    //   }
+    // }
+  }
+  sc_stop();
+}
